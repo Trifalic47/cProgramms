6ff7c05b4635e03d09502ab9bf26515b26f7991f
Trifalic47 <r00tgrep99@gmail.com>
2026-03-04 13:11:04 +0530

    Added day12/arrays.c where I learnt how to pass an array to an function using the pointers concept

--- day12/arrays.c
+++ day12/arrays.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void printContents(int *arr,int size);
+
 int main(){
     int arr[5];
     int size = sizeof(arr) / sizeof(arr[0]);
@@ -10,5 +12,14 @@ int main(){
     int *ptr = arr;
     printf("The memory address of the array is %p\n",ptr);
 
+    printContents(arr,size);
+
     return 0;
+}
+
+
+void printContents(int *arr,int size){
+    for (int i = 0 ;i < size;i++) {
+        printf("%d\n",arr[i]);
+    }
 }
